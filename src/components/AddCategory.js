b6d3b212d7c6116b662bab41a088ed72781ca983
Marco A. Ciau <marco@makerlab.mx>
2021-12-04 21:19:04 -0600

    addCategory component added

---
+++ src/components/AddCategory.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./AddCategory.css";
+export const AddCategory = ({ updateCategoryHandler }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const userInputHandler = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    if (inputValue.length > 2) {
+      updateCategoryHandler(inputValue);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <form onSubmit={submitFormHandler}>
+      <input type="text" value={inputValue} onChange={userInputHandler}></input>
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  updateCategoryHandler: PropTypes.func.isRequired,
+};
+
+export default AddCategory;
