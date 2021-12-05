f57dcddcd22638edd23d1279e9df09177cf1c8a4
Marco A. Ciau <marco@makerlab.mx>
2021-12-05 13:44:36 -0600

    adding animarions

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import GifGridItem from "./GifGridItem";
 import useFetchGifs from "../hooks/useFetchGifs";
+import 'animate.css';
 import "./GifGrid.css";
 const GifGrid = ({ category }) => {
-  const { data: imageList, loading } = useFetchGifs(category);
+  const { data: imageList } = useFetchGifs(category);
   return (
     <div>
       <h2>{category}</h2>
-      <div className="container">
-        {loading && <p>Loading</p>}
+      <div className="container  animate__animated animate__animated">
         {imageList.map((img) => {
           return <GifGridItem key={img.id} {...img} />;
         })}
--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -1,8 +1,10 @@
 import React from "react";
+import 'animate.css';
 import "./GifGridItem.css"
+
 function GifGridItem({title, url}) {
   return (
-    <div className="card">
+    <div className="card animate__animated animate__fadeIn">
       <img src={url} alt={title} />
       <p>{title}</p>
     </div>
