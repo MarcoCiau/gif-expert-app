56b677210fa55b53de21e7e8484795859a6339e7
Marco A. Ciau <marco@makerlab.mx>
2021-12-04 22:34:15 -0600

    render GifGridComponent

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,25 +1,32 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import GifGridItem from "./GifGridItem";
 const GifGrid = ({ category }) => {
-  const url =
-    "https://api.giphy.com/v1/gifs/search?q=among us&limit=10&api_key=5zxfHyHiOFKQiRNu0sm1JRYsVR2jzL2k";
-  const getGifs = async () => {
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() => {
+    getGifs("goku", "5zxfHyHiOFKQiRNu0sm1JRYsVR2jzL2k", 10);
+  }, []);
+
+  const getGifs = async (category, apiKey, limit) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=${limit}&api_key=${apiKey}`;
     const response = await fetch(url);
     const responseJSON = await response.json();
-    const gifs = responseJSON.data.map((data) => {
+    const images = responseJSON.data.map((data) => {
       return {
         id: data.id,
         title: data.title,
         url: data.images.downsized_medium.url,
       };
     });
-    console.log(gifs);
+    setImageList(images);
   };
 
-  getGifs();
   return (
     <div>
       <h2>{category}</h2>
+      {imageList.map((img) => {
+        return <GifGridItem key={img.id} {...img} />;
+      })}
     </div>
   );
 };
