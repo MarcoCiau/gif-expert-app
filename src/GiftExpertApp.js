d719879140cac7cd91eaccb3dd87bb5e91482d73
Marco A. Ciau <marco@makerlab.mx>
2021-12-04 20:11:34 -0600

    render categories array & add new category

--- src/GiftExpertApp.js
+++ src/GiftExpertApp.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function GiftExpertApp() {
+  const [categories, setCategories] = useState([
+    "Goku",
+    "One Punch",
+    "Mario Bros",
+  ]);
+  const addCategoryEventHandler = (event) => {
+    event.preventDefault();
+    setCategories((lastValue) => {
+      return [...lastValue, "Sonic"];
+    });
+  };
+
   return (
     <>
       <h2>Gift Expert App</h2>
       <hr />
+      <button onClick={addCategoryEventHandler}>Add Category</button>
+      <ul>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ul>
     </>
   );
 }
