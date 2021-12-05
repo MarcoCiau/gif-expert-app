bc109b6229ddeab8cedb695895824853290ed4e6
Marco A. Ciau <marco@makerlab.mx>
2021-12-05 13:25:05 -0600

    using custom hooks

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,26 +1,14 @@
 import React, { useState, useEffect } from "react";
 import GifGridItem from "./GifGridItem";
-import { getGifs } from "../utils/getGifs";
+import useFetchGifs from "../hooks/useFetchGifs";
 import "./GifGrid.css";
 const GifGrid = ({ category }) => {
-  const [imageList, setImageList] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const gifList = await getGifs(
-        category,
-        "5zxfHyHiOFKQiRNu0sm1JRYsVR2jzL2k",
-        10
-      );
-      setImageList(gifList);
-    }
-    fetchData();
-  }, [category]);
-
+  const { data: imageList, loading } = useFetchGifs(category);
   return (
     <div>
       <h2>{category}</h2>
       <div className="container">
+        {loading && <p>Loading</p>}
         {imageList.map((img) => {
           return <GifGridItem key={img.id} {...img} />;
         })}
