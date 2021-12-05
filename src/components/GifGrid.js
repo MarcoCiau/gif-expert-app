596ad34342fc6b3c0cafcef3ab1da214c7f2138c
Marco A. Ciau <marco@makerlab.mx>
2021-12-05 12:50:10 -0600

    update util function

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,26 +1,21 @@
 import React, { useState, useEffect } from "react";
 import GifGridItem from "./GifGridItem";
+import { getGifs } from "../utils/getGifs";
 import "./GifGrid.css";
 const GifGrid = ({ category }) => {
   const [imageList, setImageList] = useState([]);
 
   useEffect(() => {
-    getGifs("goku", "5zxfHyHiOFKQiRNu0sm1JRYsVR2jzL2k", 10);
-  }, []);
-
-  const getGifs = async (category, apiKey, limit) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=${limit}&api_key=${apiKey}`;
-    const response = await fetch(url);
-    const responseJSON = await response.json();
-    const images = responseJSON.data.map((data) => {
-      return {
-        id: data.id,
-        title: data.title,
-        url: data.images.downsized_medium.url,
-      };
-    });
-    setImageList(images);
-  };
+    async function fetchData() {
+      const gifList = await getGifs(
+        category,
+        "5zxfHyHiOFKQiRNu0sm1JRYsVR2jzL2k",
+        10
+      );
+      setImageList(gifList);
+    }
+    fetchData();
+  }, [category]);
 
   return (
     <div>
